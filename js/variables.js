4a834d3ad9c7b5fed9eadbbd95f8197d6187320a
Diego <diegocornet2020rd@gmail.com>
2023-10-26 10:44:55 -0300

    Agregamos documentos write y alertas a nuestro js

--- js/variables.js
+++ js/variables.js
@@ -19,4 +19,11 @@ console.log("Año actual: " + anioActual + " hola mundo");
 
 anioActual = 2023 + 1;
 console.log("Año actual: " + anioActual);
-console.log(url)
+console.log(url);
+
+document.write("<h1 class='text-danger'>Hola mundo " + anioActual + "</h1>");
+document.write("hola mundito </br>");
+document.write("hola mundito");
+
+//ventanas emergentes
+alert("esto es un ejemplo de una alertita");
