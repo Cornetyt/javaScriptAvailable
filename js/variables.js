7210467f681548ca0ffd4264e7875bfbe3e45250
Diego <diegocornet2020rd@gmail.com>
2023-10-26 09:25:21 -0300

    Agregamos comentarios y nuestro primer let

---
+++ js/variables.js
@@ -0,0 +1,9 @@
+/*hola a todos
+soy un comentario
+en multiples lineas*/
+
+//hola soy un comentario de una unica linea//
+
+//declarar una variable ( var[ya no se usa], let, const)
+
+let anioActual = 2023
